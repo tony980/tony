12d9893bba623c4ed7b92863db7f879054cc49f9
kaita tony <tony980>
2022-11-22 18:24:14 +0300

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,51 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Tony Wekesa",
+    "role": "web developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like to craft quality products with great user experience.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/tony-wekesa-6689541b1/",
+          "className":"fa fa-linkedin"
+        },
+       
+      ],
+    "aboutme":"A technology enthusiast.Motivated by the need for more reliability,security and efficiency in our ecosystems by constantly bringing out innovativeness within ICT spaces",
+    "email":"Tony.k.wekesa@gmail.com.",
+    "address":"Nairobi,Kenya.",
+    
+   
+   
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"PHP"
+      },
+      {
+        "skillname":"Bootstrap"
+      }
+      ,
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+   
+    "testimonials":[
+      {
+        "description":"Other Skills:",
+        "name":"Systems and Networks."
+      },
+      {
+        "description":"Other Skills:",
+        "name":"Cloud Computing."
+      }
+    ]
+  }
+  
+  export default resumeData
