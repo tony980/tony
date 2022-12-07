4552276b44d6fa5e524e5cf28d37afe3b85ea728
tony980 <61837873+tony980@users.noreply.github.com>
2022-12-07 08:31:57 +0300

    Updates

---
+++ precache-manifest.f209c682d6ddc21b7b75bdb735f1473a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42459f6df69f844c7a15",
+    "url": "/tony-port/static/css/main.bb00bb48.chunk.css"
+  },
+  {
+    "revision": "42459f6df69f844c7a15",
+    "url": "/tony-port/static/js/main.02e6dc84.chunk.js"
+  },
+  {
+    "revision": "cfc738a72c8d696fee71",
+    "url": "/tony-port/static/js/runtime~main.df0106f7.js"
+  },
+  {
+    "revision": "b1773f8afd9fb6a0106e",
+    "url": "/tony-port/static/js/2.0071b55f.chunk.js"
+  },
+  {
+    "revision": "3dfb607c18f9feaacc47abd19c2926d8",
+    "url": "/tony-port/index.html"
+  }
+];
